cb6aa3ffbc22e50926c4e7a59ee100e8037cdd04
karnauhmax <krnhmx@gmail.com>
2023-10-20 19:54:13 +0300

    fixes

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,13 @@ import "./_functions";
 import "./_components";
 import AOS from "aos";
 
-
 AOS.init({
-  once: true
+  once: true,
 });
 
-if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
-  document.body.classList.add('ios');
+if (
+  /iPhone|iPad|iPod/i.test(navigator.userAgent) ||
+  (navigator.userAgent.match(/Mac/i) && "ontouchend" in document)
+) {
+  document.body.classList.add("ios");
 }
